7a9e5b656a963f8862b1bc55645a731f155ba18b
phu1612508 <43996827+phu1612508@users.noreply.github.com>
2020-10-14 11:07:14 +0700

    Creating a Layout Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import React from 'react';
+import Layout from './components/layout/layout';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div>
+			<Layout>
+				<p>Test</p>
+			</Layout>
+		</div>
+	);
 }
 
 export default App;
