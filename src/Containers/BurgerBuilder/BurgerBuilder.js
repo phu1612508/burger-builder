bbf3360b6ac4e484dc6cc6b5b5b7e983ae975a2e
phu1612508 <43996827+phu1612508@users.noreply.github.com>
2020-10-16 10:35:35 +0700

    Connecting State to Build Controls

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 import Aux from '../../Auxiliary/Auxiliary';
 import Burger from '../../Components/Burger/Burger';
+import BuildControls from '../../Components/Burger/BuildControls/BuildControls';
+const INGREDIENT_PRICES = {
+	salad: 0.5,
+	bacon: 0.6,
+	cheese: 0.4,
+	meat: 1.3,
+};
 class BurgerBuilder extends Component {
 	state = {
 		ingredients: {
-			salad: 1,
-			bacon: 1,
-			cheese: 1,
-			meat: 2,
+			salad: 0,
+			bacon: 0,
+			cheese: 0,
+			meat: 0,
 		},
+		totalPrice: 4,
 	};
+
+	addIngredientHandler = (type) => {
+		const oldIngredientCount = this.state.ingredients[type];
+		const newIngredientCount = oldIngredientCount + 1;
+		const updateIngredient = { ...this.state.ingredients };
+		updateIngredient[type] = newIngredientCount;
+
+		const oldTotalPrice = this.state.totalPrice;
+		const additionalPrice = INGREDIENT_PRICES[type];
+		const newTotalPrice = oldTotalPrice + additionalPrice;
+
+		this.setState({ totalPrice: newTotalPrice, ingredients: updateIngredient });
+	};
+
+	removeIngredientHandler = (type) => {};
+
 	render() {
 		return (
 			<Aux>
 				<Burger ingredients={this.state.ingredients} />
-				<div>Build controls</div>
+				<BuildControls
+				ingredientsAdded={this.addIngredientHandler}
+				/>
 			</Aux>
 		);
 	}
--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,14 +2,20 @@ import React from 'react';
 import classes from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 const burger = (props) => {
-	const transformedIngredient = Object.keys(props.ingredients).map((igKey) => {
-		return [...Array(props.ingredients[igKey])].map((_, i) => {
-			return <BurgerIngredient key={igKey + i} type={igKey} />;
-		});
-	});
+	let transformedIngredient = Object.keys(props.ingredients)
+		.map((igKey) => {
+			return [...Array(props.ingredients[igKey])].map((_, i) => {
+				return <BurgerIngredient key={igKey + i} type={igKey} />;
+			});
+		})
+		.reduce((arr, el) => {
+			return arr.concat(el);
+		}, []);
+	if (transformedIngredient.length === 0) {
+		transformedIngredient = <div>Please add ingredient</div>;
+	}
 	return (
 		<div className={classes.Burger}>
-			<div>{transformedIngredient}</div>
 			<BurgerIngredient type='bread-top' />
 			{transformedIngredient}
 			<BurgerIngredient type='bread-bottom' />
